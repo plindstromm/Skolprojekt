7eed5e7ac3d143b9dd3727e06e19df64db3efbac
plindstromm <mukkepp@gmail.com>
2022-09-22 17:30:08 +0200

    fixed margins

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
+
+
+function myFunction() {
+var element = document.getElementsByClassName("darklight");
+for(let i = 0; i < element.length; i += 1){
+element.item(i).classList.toggle("dark-mode");
+  }
+console.log(element)
+//element.classList.toggle("dark-mode");
+  }
+   
