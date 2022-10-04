c3e5caceaffe0d8ef97a1a3094e1fe482c63db49
plindstromm <mukkepp@gmail.com>
2022-10-04 19:36:38 +0200

    form fix

--- script.js
+++ script.js
@@ -15,3 +15,28 @@ function LengthConverter(valNum) {
     var element = document.body;
     element.classList.toggle("dark-mode");
  }
+
+ function validateForm() {
+  let x = document.forms["myForm"]["fname"].value;
+  let y = document.forms["myForm"]["lname"].value;
+  let z = document.forms["myForm"]["tel"].value;
+  let w = document.forms["myForm"]["email"].value;
+
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+  else if (y == "") {
+    alert("Last name must be filled out");
+    return false;
+  }
+  else if (z == "") {
+    alert("Telephone must be filled out");
+    return false;
+  }
+  else if (w == "") {
+    alert("Email must be filled out");
+    return false;
+  }
+
+}
